92270dbe00a49b265ee8bb4ade4c2eea772d04fb
Luís Paulo Trevisan <lpst@cin.ufpe.br>
2025-02-08 03:18:34 -0300

    refactor all code

--- include/header.h
+++ include/header.h
@@ -1,27 +1,17 @@
 #ifndef HEADER_H
 #define HEADER_H
 
-#include <mbed.h>
-
-#define PI 3.14159265358979323846
+#include <MPU6050.h>
 
 #define PIN_SDA PF_0
 #define PIN_SCL PF_1
 
-#define GYRO_UPDATE_PERIOD 0.005
-#define GYRO_BIAS 0.003
-#define GYRO_MEASURE 0.03
+#define DEG_TO_RAD 0.0174533f  // Conversion factor from degrees to radians
+#define SAMPLE_TIME 0.005f      // 5ms sample time
+#define ALPHA 0.8f             // Low-pass filter coefficient
 
-class KalmanFilter {
-    public:
-        KalmanFilter(double, double);
-        double update(double, Timer);
-
-    private:
-        double m_Q_bias, m_R_measure, m_P[2][2]; 
-        double m_K_value, m_K_bias;
-};
-
-void read_csv(double *);
+void flag_gyro();
+void read_gyro();
+void filter_gyro();
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,45 @@
-#include <MPU6050.h>
-#include <rtos.h>
 #include <header.h>
-#include <fstream>
-#include <condition_variable>
+#include <chrono>
 
-using namespace std;
+MPU6050 mpu(PIN_SDA, PIN_SCL); // Assuming I2C is connected to PF_0 (SDA) and PF_1 (SCL)
+Ticker gyroTicker;
+volatile double angular_velocity = 0.0f;
+volatile double angle = 0.0f;
+volatile double gyro_z_rad = 0.0f;
+volatile bool is_ready = false;
 
-MPU6050 mpu(PIN_SDA, PIN_SCL);
-KalmanFilter kf(GYRO_BIAS, GYRO_MEASURE);
-Timer gyro_time;
+int main() {
+    mpu.initialize();
+    gyroTicker.attach(&flag_gyro, SAMPLE_TIME);
+    
+    while (true) {
+        if(is_ready) {
+            read_gyro();
+            filter_gyro();
+        }
+        // Integrate to get angular position (simple Euler integration)
+        angle += angular_velocity * SAMPLE_TIME;
 
-int main()
-{
-    if(!mpu.initialize()) {
-        return 1;
+        printf("Angular Position (rad): %f\n", angle);
+        ThisThread::sleep_for(4ms);
     }
-
-    Ticker gyro_update;
-    double gyro_move, gyro_z_axis = 0;
-    Thread gyro_measure_csv;
-    Mutex gyro_write;
-    ofstream output_file("output.txt");
-    string line, cell;
-
-    if(!output_file) {
-        return 1;
-    }
-
-    gyro_time.start();
-
-    /*gyro_update.attach([&]() -> double { 
-        double gyro[3];                              
-        mpu.readGyro(gyro);                       
-        gyro_z_axis = gyro[2] * PI / 180.0;
-    }, GYRO_UPDATE_PERIOD);*/
-
-    gyro_measure_csv.start(callback(read_csv, &gyro_z_axis));
-
-    while(1) {
-        gyro_move = kf.update(gyro_z_axis, gyro_time);
-
-        output_file << gyro_move << "\n";
-
-        gyro_time.reset();
-    }
-
-    output_file.close();
 
     return 0;
+}
+
+void flag_gyro() {
+    is_ready = true;
+}
+
+void read_gyro() {
+    double gyro_data[3] = {0};
+    mpu.readGyro(gyro_data);
+    
+    // Convert gyro data from degrees/sec to radians/sec
+    gyro_z_rad = gyro_data[2] * DEG_TO_RAD;
+}
+
+void filter_gyro() {
+    // Apply simple low-pass filter
+    angular_velocity = (ALPHA * angular_velocity) + ((1 - ALPHA) * gyro_z_rad);
 }
