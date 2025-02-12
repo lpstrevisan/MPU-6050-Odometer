123fd176f57642363869cf466a5833a2a5b4203b
Luís Paulo Trevisan <lpst@cin.ufpe.br>
2025-02-12 01:28:07 -0300

    chore: fiz calculate angle

--- include/header.h
+++ include/header.h
@@ -2,24 +2,22 @@
 #define HEADER_H
 
 #include <MPU6050.h>
-#include <chrono>
 
 #define PIN_SDA PF_0
 #define PIN_SCL PF_1
 
-#define DEG_TO_RAD 0.0174533   // Conversion factor from degrees to radians
+#define PI 3.14159265358979323846
 #define ALPHA 0.8              // Low-pass filter coefficient
-#define CALIBRATE_SAMPLES 100
+#define CALIBRATE_SAMPLE 100
 
 void calibrate_gyro();
-void flag_gyro();
 void read_gyro();
 void filter_gyro();
+void limit_angle();
 
-volatile double angular_velocity = 0.0;
-volatile double angle = 0.0;
-volatile double gyro_z_rad = 0.0;
-volatile bool is_ready = false;
+double angular_velocity = 0.0;
+double angle = 0.0;
+double gyro_z_rad = 0.0;
 double gyro_offset_z;
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,23 @@
 const auto sample_time = 5ms;
 
 MPU6050 mpu(PIN_SDA, PIN_SCL); // Assuming I2C is connected to PF_0 (SDA) and PF_1 (SCL)
-Ticker gyroTicker;
+
 
 int main() {
     mpu.initialize();
     calibrate_gyro();
-
-    gyroTicker.attach(&flag_gyro, sample_time);
     
     while (true) {
-        if(is_ready) {
-            is_ready = false;
-            read_gyro();
-            filter_gyro();
-        }
+        read_gyro();
+        filter_gyro();
 
         // Integrate to get angular position (simple Euler integration)
-        angle = angular_velocity * (sample_time.count() / 1000.0) ;
+        angle += angular_velocity * (sample_time.count() / 1000.0);
+        limit_angle();
+
         printf("Angular Position (rad): %f\n", angle);
 
-        ThisThread::sleep_for(sample_time - 1ms);
+        ThisThread::sleep_for(sample_time);
     }
 
     return 0;
@@ -30,29 +27,34 @@ int main() {
 
 
 void calibrate_gyro() {
-    int16_t gyro_raw[3];
-    double sum = 0;
+    double gyro_raw[3], sum = 0;
 
-    for(int i = 0; i < CALIBRATE_SAMPLES; i++) {
-        mpu.readGyroRaw(gyro_raw);
+    for(int i = 0; i < CALIBRATE_SAMPLE; i++) {
+        mpu.readGyro(gyro_raw);
         sum += gyro_raw[2];
     }
 
-    gyro_offset_z = (sum / CALIBRATE_SAMPLES) * DEG_TO_RAD;
-}
-
-void flag_gyro() {
-    is_ready = true;
+    gyro_offset_z = sum / CALIBRATE_SAMPLE;
 }
 
 void read_gyro() {
-    double gyro_data[3] = {0};
+    double gyro_data[3];
     mpu.readGyro(gyro_data);
     
-    gyro_z_rad = (gyro_data[2] - gyro_offset_z) * DEG_TO_RAD;
+    gyro_z_rad = (gyro_data[2] - gyro_offset_z) * (PI / 180.0);
 }
 
 void filter_gyro() {
     // Apply simple low-pass filter
     angular_velocity = (ALPHA * angular_velocity) + ((1 - ALPHA) * gyro_z_rad);
+}
+
+//limit an angle between -PI and +PI
+void limit_angle() {
+    if(angle > PI) {
+        angle -= 2.0 * PI;
+    }
+    else if(angle < -PI) {
+        angle += 2.0 * PI;
+    }
 }
